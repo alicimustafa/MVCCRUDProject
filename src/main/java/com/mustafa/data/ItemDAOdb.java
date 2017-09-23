fc941e57b1406156cac2db450edf1a931a2583f6
mustafa alici <mustafaalici@mustafas-MacBook-Pro.local>
2017-09-23 04:25:22 -0600

    fineshed getItemByType mehtod in itemDAOdb class

--- src/main/java/com/mustafa/data/ItemDAOdb.java
+++ src/main/java/com/mustafa/data/ItemDAOdb.java
@@ -1,9 +1,18 @@
 package com.mustafa.data;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.stereotype.Component;
 
 import com.mustafa.model.Item;
 
+@Component
 public class ItemDAOdb implements ItemDAO {
 	
 	private static String url = "jdbc:mysql://localhost:3306/partydb";
@@ -18,11 +27,49 @@ public class ItemDAOdb implements ItemDAO {
 			System.err.println("Error loading MySQL Driver!!!");
 		}
 	}
-	
+
 	@Override
-	public List<Item> getItemByType(String type) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Item> getItemByType(int type) {
+		List<Item> items = new ArrayList<>();
+		try {
+			Connection conn = DriverManager.getConnection(url, user, pass);
+			String sql = "SELECT id, name FROM items WHERE type = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, type);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				items.add(new Item(id, name, type));
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return items;
+	}
+
+	@Override
+	public Item getItemById(int index) {
+		Item item = null;
+		try {
+			Connection conn = DriverManager.getConnection(url, user, pass);
+			String sql = "SELECT title FROM film WHERE id = ?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				title = rs.getString(1);
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return title;
 	}
 
 	@Override
@@ -40,19 +87,13 @@ public class ItemDAOdb implements ItemDAO {
 	@Override
 	public void setItemList(List<Item> itemList) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 	@Override
 	public void addNewItem(Item item) {
 		// TODO Auto-generated method stub
-
+		
 	}
-
-	@Override
-	public Item getItemById(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
